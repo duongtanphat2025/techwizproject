e971802aeb75ce9745b1b55718af99be8f510e7c
duongtanphat2025 <duongtanphat2019@gmail.com>
2024-09-20 11:26:13 +0700

    update Hung Source

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
           <Route path="/gallery" element={<Gallery/>}/>
           <Route path="/feedback" element={<Feedback/>}/>
           <Route path="/search" element={<Search/>}/>
+          <Route path="*" element={<Home/>}/>
         </Routes>
       </BrowserRouter>
     </>
