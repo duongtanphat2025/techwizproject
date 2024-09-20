8a2747b715204ba533577b4619eecdaff596f8d8
duongtanphat2025 <duongtanphat2019@gmail.com>
2024-09-20 10:19:44 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Gallery from "./components/Gallery";
 import Feedback from "./components/Feedback";
 import Search from "./components/Search";
 const App = () => {
-  return ( 
+  return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/gallery" element={<Gallery/>}/>
-          <Route path="/feedback" element={<Feedback/>}/>
-          <Route path="/search" element={<Search/>}/>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/about" element={<About />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
-   );
-}
+  );
+};
 export default App;
